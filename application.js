815362472831325a2c38fde4054663a16b335e1d
Anass <sebaianass@gmail.com>
2023-02-28 17:19:38 +0100

    s

--- application.js
+++ application.js
@@ -1,12 +1,54 @@
 const fs = require('fs');
 let rawdata = fs.readFileSync('users.json');
+//on a recupéré les données du fichier et on les stocke dans users
 let users = JSON.parse(rawdata);
-console.log(users);
+let country = new Array(users.length);
+let company = new Array(users.length);
 
-//afficher les pays sans repetition 
-const pays = Array.from(new Set(users.map(user => user.country)));
-console.log(pays);
-//afficher les societe sans repetition
-const societe = Array.from(new Set(users.map(user => user.company)));
-console.log(societe);
+console.log("       Menu : \n");
+console.log(" -- Afficher la liste des pays et le compteur");
+console.log(" -- Afficher la liste des sociétés et le compteur");
+const input = process.argv[2];
 
+if (input === 'country') {
+    //on met que les country dans country
+    for (let i = 0; i < users.length; i++) {
+        country[i] = users[i].country;
+    }
+    // on compte les country et on les met dans l'objet counts
+    const counts = {};
+    for (const num of country) {
+        counts[num] = counts[num] ? counts[num] + 1 : 1;
+    }
+
+    let res = [];
+    // on transforme l'objet counts en tableau
+    for (let i in counts) {
+        res.push({ "country": i, "count": counts[i] });
+    }
+
+    //on sort le tableau
+    res.sort((a, b) => b.count - a.count);
+    console.log(res);
+
+}else if(input === 'company'){
+    //on met que les company dans company
+    for (let i = 0; i < users.length; i++) {
+        company[i] = users[i].company;
+    }
+    // on compte les company et on les met dans l'objet counts
+    const counts = {};
+    for (const num of company) {
+        counts[num] = counts[num] ? counts[num] + 1 : 1;
+    }
+
+    let res = [];
+    // on transforme l'objet counts en tableau
+    for (let i in counts) {
+        res.push({ "company": i, "count": counts[i] });
+    }
+
+    //on sort le tableau
+    res.sort((a, b) => b.count - a.count);
+    console.log(res);
+}
